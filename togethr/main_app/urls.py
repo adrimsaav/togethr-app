ba0108fea3a9291b5b73595ad042537e8a486698
adrimsaav <adrimsaav@gmail.com>
2023-11-22 18:10:25 -0500

    fix login and sign-up paths

--- togethr/main_app/urls.py
+++ togethr/main_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 	
 urlpatterns = [
   path('', views.home, name='home'),
+  path('accounts/login/', views.login, name='login'),
   path('accounts/signup/', views.signup, name='signup'),
-
 ]
