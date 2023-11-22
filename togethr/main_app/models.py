a345a3b51b8b69470acfbdd8d3cb13b936cae0f1
adrimsaav <adrimsaav@gmail.com>
2023-11-22 16:57:13 -0500

    create landing page

--- togethr/main_app/models.py
+++ togethr/main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User(models.Model):
+    # user_id FK
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwagrs={'cat_id': self.id})
+    
