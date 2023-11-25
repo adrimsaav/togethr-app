386d757b71b93341473351b1417f85db52b69ea2
adrimsaav <adrimsaav@gmail.com>
2023-11-25 15:20:28 -0500

    fix login and signup paths

--- togethr/main_app/models.py
+++ togethr/main_app/models.py
@@ -4,13 +4,14 @@ from datetime import date
 from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    # user_id FK
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Profile(models.Model):
+    # profile that user can also delete
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # be able to follow different profiles, related_name is to see who is following who
+    # 'symmetrical' is for when whoever User follows, other User does not have to follow them.
+    # 'blank' is for when you have 0 following
+    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False, blank=True)
 
     def __str__(self):
-        return f'{self.name} ({self.id})'
-    
-    def get_absolute_url(self):
-        return reverse('detail', kwagrs={'cat_id': self.id})
-    
+        return self.user.username
+
