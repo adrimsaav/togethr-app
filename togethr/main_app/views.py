eae2e9e0140419a6e1b7b1082bc70656dc6e0cc4
adrimsaav <adrimsaav@gmail.com>
2023-11-28 15:14:03 -0500

    add paths to account settings

--- togethr/main_app/views.py
+++ togethr/main_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
   return render(request, 'home.html')
@@ -19,12 +22,34 @@ def signup(request):
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
 
+# account settings
+@login_required
+def edit_posts(request):
+    return render(request, 'menu/account/edit_posts.html')
+
+@login_required
+def edit_profile(request):
+    return render(request, 'menu/account/edit_profile.html')
+
+@login_required
+def delete_profile(request):
+    return render(request, 'menu/account/delete_profile.html')
+
+
+# nav menu
+@login_required
 def profile(request):
     return render(request, 'menu/profile.html')
 
+@login_required
 def account_settings(request):
     return render(request, 'menu/account_settings.html')
 
+@login_required
 def profile_list(request):
-    return render(request, 'menu/profile_list.html') 
+    profiles = Profile.objects.exclude(user=request.user)
+
+    return render(request, 'menu/profile_list.html', {"profiles": profiles}) 
+
+
 
