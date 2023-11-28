74d33b37cce929c9793d7ddaaaab63e1ecdbd38c
root <root@DESKTOP-PV5G1DG.>
2023-11-28 17:40:27 -0500

    Set up pk ID for user profile page

--- togethr/main_app/views.py
+++ togethr/main_app/views.py
@@ -24,6 +24,29 @@ def signup(request):
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
 
+
+def profile_list(request):
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'menu/profile_list.html', {"profiles":profiles})
+    else:
+        messages.success(request, ("Please Log In Or Sign Up To View This Page"))
+        return redirect('home')
+
+
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+        return render(request, 'menu/profile.html', {"profile":profile})
+    else:
+        messages.success(request, ("Please Log In Or Sign Up To View This Page"))
+        return redirect('home')
+
+
+def account_settings(request):
+    return render(request, 'menu/account_settings.html')
+
+
 # account settings
 @login_required
 def edit_posts(request):
@@ -39,9 +62,6 @@ def delete_profile(request):
 
 
 # nav menu
-@login_required
-def profile(request):
-    return render(request, 'menu/profile.html')
 
 @login_required
 def account_settings(request):
@@ -56,19 +76,4 @@ def profile_list(request):
 
 
 
-def profile_list(request):
-    if request.user.is_authenticated:
-        profiles = Profile.objects.exclude(user=request.user)
-        return render(request, 'menu/profile_list.html', {"profiles":profiles})
-    else:
-        messages.success(request, ("Please Log In Or Sign Up To View This Page"))
-        return redirect('home')
-
-
-def profile(request):
-    return render(request, 'menu/profile.html')
-
-
-def account_settings(request):
-    return render(request, 'menu/account_settings.html')
 
