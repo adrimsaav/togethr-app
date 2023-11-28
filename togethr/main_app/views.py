77d524bd919a8ea5e089a8a3b8f6c2396413ed06
Kevintrxn <kevintran12kt@gmail.com>
2023-11-28 15:49:22 -0600

    Fix users being redirected when posting

--- togethr/main_app/views.py
+++ togethr/main_app/views.py
@@ -25,26 +25,16 @@ def profile_list(request):
     return render(request, 'profile_list.html') 
 
 def home_view(request):
-    if request.method == 'POST' and request.user.is_authenticated:
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.user = request.user
-            post.save()
-            return redirect('home')
-    else:
-        form = PostForm()
-    posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'home.html', {'form': form, 'posts': posts})
-
-def post_create(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
             new_post = form.save(commit=False)
-            new_post.user = request.user  
+            new_post.user = request.user
             new_post.save()
-            return redirect('home')  
+            return redirect('home') 
     else:
-        form = PostForm() 
-    return render(request, 'post_create.html', {'form': form}) 
+        form = PostForm()
+    
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'form': form, 'posts': posts})
+
