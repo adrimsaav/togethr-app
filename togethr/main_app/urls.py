d8dd8550cb678c3955b6b21e52ebed8ddc313664
adrimsaav <adrimsaav@gmail.com>
2023-11-28 10:19:55 -0500

    fix paths for logo and nav menu

--- togethr/main_app/urls.py
+++ togethr/main_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+
 	
 urlpatterns = [
   path('', views.home, name='home'),
   path('accounts/signup/', views.signup, name='signup'),
-  path('profile_list/', views.profile_list, name='profile_list'),
+  path('menu/profile/', views.profile, name='profile'),
+  path('menu/profile_list/', views.profile_list, name='profile_list'),
+  path('menu/account_settings/', views.account_settings, name='account_settings'),
 ]
