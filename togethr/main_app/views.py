fac698d7daade5c0887329e39750a3a004de7eac
root <root@DESKTOP-PV5G1DG.>
2023-11-28 11:59:51 -0500

    Update profile list and create error if user does not have an account

--- togethr/main_app/views.py
+++ togethr/main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
+from django.contrib import messages
 
 def home(request):
   return render(request, 'home.html')
@@ -21,6 +22,10 @@ def signup(request):
     return render(request, 'registration/signup.html', context)
 
 def profile_list(request):
-    profiles = Profile.objects.exclude(user=request.user)
-    return render(request, 'profile_list.html', {"profiles":profiles}) 
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'profile_list.html', {"profiles":profiles})
+    else:
+        messages.success(request, ("Please Log In Or Sign Up To View This Page"))
+        return redirect('home')
 
