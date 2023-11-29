bbbdfa12e3ead50384c2dfc15d5c8eddfb7a2236
root <root@DESKTOP-PV5G1DG.>
2023-11-29 14:50:24 -0500

    Fix signup error

--- togethr/main_app/views.py
+++ togethr/main_app/views.py
@@ -17,7 +17,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('index')
+            return redirect('home')
         else:
             error_message = 'Invalid sign up - try again'
     form = UserCreationForm()
